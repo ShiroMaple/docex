dd607be0ee714cb9e0e56a8020aecb13fff5b9bb
白枫 <shiromaple@gmail.com>
2026-07-14 15:31:17 +0800

    ci: 添加 GitHub Actions 部署工作流与 PM2 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "docex", // 你的项目名称
+            script: "./.next/standalone/server.js", // 启动脚本指向 standalone 的入口
+            env: {
+                PORT: 4003, // 对应项目的端口
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
